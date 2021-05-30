a34437a04af4c38cdff498ecaa8db8933cef257b
seray <yagciseray@hotmail.com>
2021-05-31 01:15:16 +0300

    fd

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,188 +1,170 @@
 const data = {
     products: [
        {
-                "_id": "60b26c2d0633c3224035e7d6",
+                "id": "60b26c2d0633c3224035e7d6",
                 "name": "Cujo",
                 "category": "Books",
-                "image": "/uploads\\1622381935750.jpg",
+                "image": "/uploads/1622381935750.jpg",
                 "price": 10,
                 "countInStock": 15,
                 "rating": 5,
                 "numReviews": 15,
                 "description": "An amazing book!!!",
                 "__v": 9,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.312Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T17:13:07.589Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.312Z"
+                ,
+                "updatedAt": "2021-05-30T17:13:07.589Z"
+                ,
                 "images": [
-                  "/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg","/uploads\\1622332838941.jpg"
+                  "/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg","/uploads/1622332838941.jpg"
                 ],
                 "active": true
               },{
-                "_id": "60b26c2d0633c3224035e7d7"
+                "id": "60b26c2d0633c3224035e7d7"
                 ,
                 "name": "The Stand",
                 "category": "Books",
-                "image": "/uploads\\1622381988971.jpg",
+                "image": "/uploads/1622381988971.jpg",
                 "price": 10,
                 "countInStock": 20,
                 "rating": 5,
                 "numReviews": 17,
                 "description": "An amazing book!",
                 "__v": 2,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.312Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T17:25:23.561Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.312Z"
+                ,
+                "updatedAt": "2021-05-30T17:25:23.561Z"
+                ,
                 "images": [
-                  "/uploads\\1622331800367.jpg"
+                  "/uploads/1622331800367.jpg"
                 ],
                 "active": true
               },{
-                "_id": "60b26c2d0633c3224035e7d8"
+                "id": "60b26c2d0633c3224035e7d8"
                 ,
                 "name": "The Shining",
                 "category": "Books",
-                "image": "/uploads\\1622381998466.jpg",
+                "image": "/uploads/1622381998466.jpg",
                 "price": 10,
                 "countInStock": 15,
                 "rating": 4,
                 "numReviews": 12,
                 "description": "An amazing book!",
                 "__v": 1,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.312Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:39:59.902Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.312Z"
+                ,
+                "updatedAt": "2021-05-30T13:39:59.902Z"
+                ,
                 "images": [],
                 "active": true
               },{
-                "_id": "60b26c2d0633c3224035e7d9"
+                "id": "60b26c2d0633c3224035e7d9"
                 ,
                 "name": "It",
                 "category": "Books",
-                "image": "/uploads\\1622382012625.jpg",
+                "image": "/uploads/1622382012625.jpg",
                 "price": 10,
                 "countInStock": 20,
                 "rating": 5,
                 "numReviews": 6,
                 "description": "An amazing book!",
                 "__v": 1,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.313Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T17:33:28.158Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.313Z"
+                ,
+                "updatedAt": "2021-05-30T17:33:28.158Z"
+                ,
                 "images": [],
                 "active": false
               },{
-                "_id":  "60b26c2d0633c3224035e7da"
+                "id":  "60b26c2d0633c3224035e7da"
                 ,
                 "name": "The Dark Tower",
                 "category": "Books",
-                "image": "/uploads\\1622382020442.jpg",
+                "image": "/uploads/1622382020442.jpg",
                 "price": 10,
                 "countInStock": 15,
                 "rating": 5,
                 "numReviews": 3,
                 "description": "An amazing book!",
                 "__v": 1,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.313Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:40:21.837Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.313Z"
+                ,
+                "updatedAt": "2021-05-30T13:40:21.837Z"
+                ,
                 "images": [],
                 "active": false
               },{
-                "_id":  "60b26c2d0633c3224035e7db"
+                "id":  "60b26c2d0633c3224035e7db"
                 ,
                 "name": "Misery",
                 "category": "Books",
-                "image": "/uploads\\1622382032674.jpg",
+                "image": "/uploads/1622382032674.jpg",
                 "price": 10,
                 "countInStock": 15,
                 "rating": 4,
                 "numReviews": 10,
                 "description": "An amazing book!",
                 "__v": 1,
-                "createdAt": {
-                  "$date": "2021-05-29T16:30:37.313Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:40:33.986Z"
-                },
+                "createdAt": "2021-05-29T16:30:37.313Z"
+                ,
+                "updatedAt": "2021-05-30T13:40:33.986Z"
+                ,
                 "images": [],
                 "active": true
               },{
-                "_id": "60b2b845e4c63836e44ff816"
+                "id": "60b2b845e4c63836e44ff816"
                 ,
                 "name": "Pet Semetary",
-                "image": "/uploads\\1622382052602.jpg",
+                "image": "/uploads/1622382052602.jpg",
                 "price": 12,
                 "category": "Books",
                 "countInStock": 3232,
                 "rating": 4,
                 "numReviews": 6,
                 "description": "A book",
-                "createdAt": {
-                  "$date": "2021-05-29T21:55:17.467Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:40:53.994Z"
-                },
+                "createdAt": "2021-05-29T21:55:17.467Z"
+                ,
+                "updatedAt": "2021-05-30T13:40:53.994Z"
+                ,
                 "__v": 1,
                 "images": [],
                 "active": true
               },{
-                "_id": "60b2b87be4c63836e44ff817"
+                "id": "60b2b87be4c63836e44ff817"
                 ,
                 "name": "Carrie",
-                "image": "/uploads\\1622382039314.jpg",
+                "image": "/uploads/1622382039314.jpg",
                 "price": 54,
-                "category": "sample category",
+                "category": "Books",
                 "countInStock": 0,
                 "rating": 3,
                 "numReviews": 12,
                 "description": "sample description",
-                "createdAt": {
-                  "$date": "2021-05-29T21:56:11.881Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:40:40.512Z"
-                },
+                "createdAt": "2021-05-29T21:56:11.881Z"
+                ,
+                "updatedAt": "2021-05-30T13:40:40.512Z"
+            ,
                 "__v": 1,
                 "images": [],
                 "active": false
               },{
-                "_id": "60b38a082ce5cb4ad06eed4e"
+                "id": "60b38a082ce5cb4ad06eed4e"
                 ,
                 "images": [
-                  "/uploads\\1622379024217.jpg"
+                  "/uploads/1622379024217.jpg"
                 ],
                 "name": "11/22/63",
-                "image": "/uploads\\1622382118134.jpg",
+                "image": "/uploads/1622382118134.jpg",
                 "price": 0,
-                "category": "sample category",
+                "category": "Books",
                 "countInStock": 0,
                 "rating": 2,
                 "numReviews": 13,
                 "description": "sample description",
-                "createdAt": {
-                  "$date": "2021-05-30T12:50:16.228Z"
-                },
-                "updatedAt": {
-                  "$date": "2021-05-30T13:41:59.695Z"
-                },
+                "createdAt": "2021-05-30T12:50:16.228Z"
+                ,
+                "updatedAt": "2021-05-30T13:41:59.695Z"
+                ,
                 "__v": 1,
                 "active": false
               }]}
