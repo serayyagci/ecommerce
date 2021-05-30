54eb4db9a6f0ac40bb786291be2aff988d8c04b5
serayyagci <69105927+serayyagci@users.noreply.github.com>
2021-05-31 02:21:15 +0300

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -1,19 +1,6 @@
 import bcrypt from 'bcryptjs';
 const data = {
-    users: [
-        {
-            name:'Seray',
-            email:'admin@example.com',
-            password: bcrypt.hashSync('1234', 8),
-            isAdmin: true,
-        },
-        {
-            name:'Client',
-            email:'user@example.com',
-            password: bcrypt.hashSync('1234', 8),
-            isAdmin: false,
-        },
-    ],
+  
     products: [
         {
         name: 'Cujo',
